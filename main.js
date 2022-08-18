3883b2b23ef361d2341670d8fc2d9ceccebf8849
feras alnowiser <ferasdev@gmail.com>
2022-08-18 23:43:21 +0300

    printing all texts of paragraphs using DOM in JavaScript

--- main.js
+++ main.js
@@ -1,3 +1,25 @@
 console.log('Week 1 - Day 1')
 
 console.log('Tuwaiq Academy   JS')
+
+// JavaScript DOM
+
+console.log('-------------------- Paragraph (1) --------------------')
+let paragraph1 = document.querySelector('#paragraph1');
+console.log(paragraph1.innerHTML);
+
+console.log('-------------------- Paragraph (2) --------------------')
+let paragraph2 = document.querySelector('#paragraph2');
+console.log(paragraph2.innerHTML);
+
+console.log('-------------------- Paragraph (3) --------------------')
+let paragraph3 = document.querySelector('#paragraph3');
+console.log(paragraph3.innerHTML);
+
+console.log('-------------------- Paragraph (4) --------------------')
+let paragraph4 = document.querySelector('#paragraph4');
+console.log(paragraph4.innerHTML);
+
+console.log('-------------------- Paragraph (5) --------------------')
+let paragraph5 = document.querySelector('#paragraph5');
+console.log(paragraph5.innerHTML);
